4adc1a14b776b783f6ad34a0615f954f1bde6185
vonavi <greenliptonfan@gmail.com>
2017-12-06 16:01:43 +0300

    Adding CategoryAction

---
+++ CategoryAction.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Валентин
+ * Date: 06.12.2017
+ * Time: 15:42
+ */
+class CategoryAction
+{
+    /**
+     * @var int
+     *
+     * required
+     * ID категории на сервере (1 – супы, 2 – горячее, 3 – салаты, 4 – десерты, 5 – напитки, 6 – завтраки, 7-n любые другие категории)
+     */
+    public $category_id;
+
+    /**
+     * @var string
+     *
+     * required
+     * Имя категории
+     */
+    public $category_name;
+
+    /**
+     * @var int
+     *
+     * 0 - remove
+     * 1 - add or update
+     */
+    public $action;
+
+    /**
+     * @var string
+     *
+     * status
+     */
+    public $status;
+}
